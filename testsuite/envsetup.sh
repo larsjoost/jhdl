3dcb74537c02eaa0a4c4dd863f635b6c1a7e4ee5
Lars Joost Jensen <ljj@terma.com>
2017-01-25 08:42:45 +0100

    All testsuites ok

--- testsuite/envsetup.sh
+++ testsuite/envsetup.sh
@@ -5,7 +5,7 @@ set -e
 SCRIPTPATH=$(dirname ${BASH_SOURCE[0]})
 
 if [ -z "$JHDL" ]; then
-    JHDL=$SCRIPTPATH/../src/jhdl
+    JHDL=$SCRIPTPATH/../src
 fi
 
 if [ -n "$VALGRIND" ]; then
@@ -21,14 +21,9 @@ else
 fi
     
 function analyse {
-    $JHDL -f $1
+    $JHDL/jhdl -f $1
 }
 
 function simulate {
-    local filename=$1
-    local filebase="${filename%.*}"
-    local cppfile="$filebase.cpp"
-    $JHDL -f $filename > $cppfile
-    g++ -std=c++11 -g  -I$SCRIPTPATH/../src/kernel/lib -o $filebase $cppfile
-    ./$filebase
+    $JHDL/hdls $1
 }
