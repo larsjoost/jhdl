7786d4fba6a46f192a299a56331bcbdd49cc52ff
Lars Joost Jensen <ljj@terma.com>
2017-03-03 14:21:07 +0100

    Extended function test

--- testsuite/success/vhdl/function/test.vhdl
+++ testsuite/success/vhdl/function/test.vhdl
@@ -7,7 +7,7 @@ use std.env.finish;
 architecture rtl of test is
 
   function func (
-    x : integer)
+    x : integer := 1)
     return integer is
   begin
     return x + 1;
@@ -18,10 +18,23 @@ begin
   process is
     variable a : integer;
   begin
-    a := 0;
+    a := func;
+    if (a /= 2) then
+      report "a = " & integer'image(a) & ", but expected 2"
+        severity failure;
+    else
+      report "a = " & integer'image(a) severity note;
+    end if;
     a := func(a);
-    if (a /= 1) then
-      report "a = " & integer'image(a) & ", but expected 1"
+    if (a /= 3) then
+      report "a = " & integer'image(a) & ", but expected 3"
+        severity failure;
+    else
+      report "a = " & integer'image(a) severity note;
+    end if;
+    a := func(x => a);
+    if (a /= 4) then
+      report "a = " & integer'image(a) & ", but expected 4"
         severity failure;
     else
       report "a = " & integer'image(a) severity note;
