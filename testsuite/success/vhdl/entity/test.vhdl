54fa29e04b4dd5a24f4823eaaf327da31d42caa5
Lars Joost Jensen <larsjoostjensen@gmail.com>
2017-03-05 19:47:09 +0100

    Extended entity test

--- testsuite/success/vhdl/entity/test.vhdl
+++ testsuite/success/vhdl/entity/test.vhdl
@@ -12,3 +12,37 @@ begin
   q <= a + b;
 
 end architecture rtl;
+
+entity test2 is
+  
+end entity test2;
+
+library std;
+use std.env.finish;
+
+architecture rtl of test2 is
+
+  signal a : integer;
+  signal b : integer;
+  signal q : integer;
+  
+begin
+
+  test_1: entity work.test
+    port map (
+      a => a,
+      b => b,
+      q => q);
+
+  process is
+  begin  -- process
+    a <= 1;
+    b <= 2;
+    wait for 1 ns;
+    if q /= 3 then
+      report "Q = " & integer'image(q) & ", but expected = 3" severity failure;
+    end if;
+    finish(0);
+  end process;
+  
+end architecture rtl;
